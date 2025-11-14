42759ae93617fe87dc343441a07fe6107e11e513
Pranav Nair <nairp2@tcnj.edu>
2025-11-14 12:08:24 -0500

    Aded build matrix yml file

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,7 @@
 def test_addition():
-    result =  1 + 1
+    result =  2 + 1
     print("Result of sum:", result)
-    assert result == 2
+    assert result == 3
 
 if __name__ == "__main__":
     print(test_addition())
